98df21a48d63b78ce3c0321094651e8080fe406f
Swapnil Pandey <swpnil.pndey@gmail.com>
2023-07-21 18:49:12 +0530

    Mozilla firefox

--- python.py
+++ python.py
@@ -1,23 +1,29 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-chrome_options = Options()
-chrome_options.add_argument("--user-data-dir=C:\\Users\\Dell\\AppData\\Local\\Google\\Chrome\\User Data\\")
-chrome_options.add_argument("--profile-directory=Default")  
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-service = Service('C:\\chromedriver_win32\\chromedriver.exe')
-driver = webdriver.Chrome(service=service, options=chrome_options)
+firefox_options = Options()
+firefox_options.add_argument("--profile=C:\\Users\\Dell\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\6vyqky34.default-release")
+
+service = Service('C:\geckodriver-v0.33.0-win32driver\geckodriver.exe')  
+
+driver = webdriver.Firefox(service=service, options=firefox_options)
 driver.get("https://sepoliafaucet.com/")
 
-eoa=driver.find_element(By.TAG_NAME,'input')
+wait = WebDriverWait(driver, 10)
+eoa = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type="text"]')))
 
 eoa.clear()
 
-eoa.send_keys("0xCa83F31f9b861E878A11efFBfB1fb38553ce7b1D")
+eoa.send_keys("0x95005C2defeae33C60432079B6b3845eC0fBfbC7")
 
-time.sleep(15)    
+time.sleep(15)
+
+
